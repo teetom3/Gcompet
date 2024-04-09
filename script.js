c0a68b57045c4cae1a379ed5c5249b55445e8e08
teetom3 <thomas.zeryouh@gmail.com>
2024-04-09 10:28:10 +0200

    html final

---
+++ script.js
@@ -0,0 +1,27 @@
+const navItems = document.querySelector('.sidebar');
+const openNavBtn = document.querySelector('.open_nav');
+const closeNavBtn = document.querySelector('.close_nav');
+
+
+const openNav = () => {
+    navItems.style.display = 'flex';
+    
+}
+
+openNavBtn.addEventListener('click', openNav);
+
+
+
+
+const closeNav = () => {
+    navItems.style.display = 'none'; 
+    
+    
+
+}
+
+closeNavBtn.addEventListener('click',closeNav );
+
+
+
+
